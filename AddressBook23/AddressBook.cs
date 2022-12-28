c9f7bfc239975b20818661773dbc992d0ce1eee1
shubham07h <shubham1609sharma@gmail.com>
2022-12-29 01:14:03 +0530

    commit

---
+++ AddressBook23/AddressBook.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookDay23
+{
+    internal class AddressBook
+    {
+        public string FirstName, LastName, Address, City, State, Email;
+        public int Zip;
+        public long PhoneNum;
+        public AddressBook(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNum)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            City = city;
+            State = state;
+            Email = email;
+            Zip = zip;
+            PhoneNum = phoneNum;
+        }
+        public void DisplayContacts()
+        {
+            Console.WriteLine("Name {0} {1}", FirstName, LastName);
+            Console.WriteLine("Address : {0}", Address);
+            Console.WriteLine("City :{0}", City);
+            Console.WriteLine("State : {0}", State);
+            Console.WriteLine("Email :{0}", Email);
+            Console.WriteLine("Zip :{0}", Zip);
+            Console.WriteLine("Phone :{0}", PhoneNum);
+        }
+    }
+}
--- AddressBook23/Program.cs
+++ AddressBook23/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookDay23
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address BOOK Program");
+            AddressBook ab = new AddressBook("shubham", "hjdhjd", "abs", "delhi", "delhi", "shubham@gmail.com", 400023, 933387412);
+            ab.DisplayContacts();
+        }
+    }
+}
